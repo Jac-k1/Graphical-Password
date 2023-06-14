f200d00fce9268e84638fbba3f68e42fa11903ec
Jac-k1 <hasakixz@gmail.com>
2023-06-14 18:31:54 -0400

    figuring out next.js
    made some pages like login, register

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import './page.css'
+
 export default function HomePage() {
   return (
       <div>
           <h1>HomePage</h1>
           <h2>contents</h2>
           <p>What is even going on</p>
+          <a href="./Login">Link to login page</a>
+          <br></br>
+          <a href='./Register'>Link to register Page</a>
       </div>
   );
 }
