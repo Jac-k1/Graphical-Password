488e54d184540ea4d48210727939ec61badef034
Jac-k1 <99202363+Jac-k1@users.noreply.github.com>
2023-06-14 14:32:55 -0400

    created next.js project

---
+++ graphical_password/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+      <div>
+          <h1>HomePage</h1>
+          <h2>contents</h2>
+          <p>What is even going on</p>
+      </div>
+  );
+}
