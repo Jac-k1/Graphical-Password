dd513293eef83bfdf50696cecaa4dc734dbfd3f8
Jac-k1 <hasakixz@gmail.com>
2023-06-18 17:09:02 -0400

    changed to just react

---
+++ public/login.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link rel='stylesheet' href='login.css'></link>
+    </head>
+    <body>
+        <div class='main'>
+            <h1>Welcome to the Login page</h1>
+
+            <form action='login.php' method='post'>
+                <p>Username</p>
+                <input type='text' name="username"></input>
+                <br></br>
+                <p>Password</p>
+                <input type='password' name="password"></input>
+                <br></br>
+                <br></br>
+                <input type='submit' value='Login'></input>
+            </form>
+        </div>
+    </body>
+</html>
