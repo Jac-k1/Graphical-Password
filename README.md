0912168165080beb128a8ed52f09da229a42bb9c
Jac-k1 <hasakixz@gmail.com>
2023-07-05 21:14:02 -0400

    fix for order

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ CREATE TABLE pokemon_sprites (
   sprite_url VARCHAR(255),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+--------------------------------------------------------
+
+CREATE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE pokemon_order (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    pokemon_id INT,
+    order_number INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $hostname = 'localhost';
 $username = 'root';
 $password = 'password';
@@ -12,14 +12,15 @@ if ($mysqli->connect_errno) {
     die("Failed to connect to MySQL: " . $mysqli->connect_error);
 }
 
-function fetchData($url) {
+function fetchData($url)
+{
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $data = curl_exec($curl);
     curl_close($curl);
     return $data;
 }
-
+/*
 // Retrieve a list of 30 Pokémon from PokeAPI
 $pokemonListUrl = 'https://pokeapi.co/api/v2/pokemon?limit=30';
 $pokemonListData = fetchData($pokemonListUrl);
@@ -32,39 +33,46 @@ foreach ($pokemonList as $pokemon) {
     $pokemonData = fetchData($pokemonUrl);
     $pokemonDetails[] = json_decode($pokemonData);
 }
+*/
+
+$url = 'https://pokeapi.co/api/v2/pokemon?limit=30';
+$pokemon_data = json_decode(file_get_contents($url), true)['results'];
+
 ?>
 
 <!-- Add a JavaScript function for form validation -->
 <script>
-function validateForm() {
-    var username = document.forms["registrationForm"]["username"].value;
-    var password = document.forms["registrationForm"]["password"].value;
-    var selectedPokemon = document.querySelectorAll('input[name="selected_pokemon[]"]:checked');
+    function validateForm() {
+        var username = document.forms["registrationForm"]["username"].value;
+        var password = document.forms["registrationForm"]["password"].value;
+        var selected_pokemon = document.querySelectorAll('input[name="selected_pokemon[]"]:checked');
 
-    if (username === "" || password === "") {
-        alert("Please fill in all fields.");
-        return false;
-    }
+        if (username === "" || password === "") {
+            alert("Please fill in all fields.");
+            return false;
+        }
 
-    if (selectedPokemon.length === 0) {
-        alert("Please select at least one Pokémon.");
-        return false;
+        if (selected_pokemon.length === 0) {
+            alert("Please select at least one Pokémon.");
+            return false;
+        }
     }
-}
 </script>
 
 <!-- Display the Pokémon sprites in boxes -->
 <form name="registrationForm" method="post" onsubmit="return validateForm();">
     Username: <input type="text" name="username"><br>
     Password: <input type="password" name="password"><br>
-    <?php foreach ($pokemonDetails as $pokemon) {
-        $pokemonSprite = $pokemon->sprites->front_default;
-        $pokemonName = $pokemon->name;
-        ?>
-        <label>
-            <input type="checkbox" name="selected_pokemon[]" value="<?php echo $pokemonSprite; ?>">
-            <img src="image-proxy.php?url=<?php echo urlencode($pokemonSprite); ?>" alt="<?php echo $pokemonName; ?>">
-        </label>
+    <?php 
+        foreach ($pokemon_data as $pokemon) {
+        $pokemon_detail = json_decode(file_get_contents($pokemon['url']), true);
+        $pokemon_name = $pokemon['name'];
+        $pokemon_sprite = $pokemon_detail['sprites']['front_default'];
+    ?>
+    <label>
+        <input type="checkbox" name="selected_pokemon[]" value="<?php echo $pokemon_detail['id']; ?>">
+        <img src="<?php echo $pokemon_sprite?>" alt="<?php echo $pokemon_name; ?>">
+    </label>
     <?php } ?>
     <br><br>
     <input type="submit" value="Submit">
@@ -80,19 +88,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
     if ($mysqli->query($query)) {
         // Get the ID of the newly inserted user
-        $userID = $mysqli->insert_id;
+        $user_ID = $mysqli->insert_id;
 
         // Retrieve the selected Pokémon sprites
-        $selectedPokemon = $_POST['selected_pokemon'];
+        $selected_pokemon = $_POST['selected_pokemon'];
 
         // Insert the selected Pokémon sprites into the database
-        foreach ($selectedPokemon as $pokemonSprite) {
+        $order = 1;
+        foreach ($selected_pokemon as $pokemon_ID) {
             // Escape the sprite URL and user ID to prevent SQL injection
-            $escapedSprite = $mysqli->real_escape_string($pokemonSprite);
+            $escaped_pokemonID = $mysqli->real_escape_string($pokemon_ID);
 
             // Insert the sprite URL and user ID into the database
-            $query = "INSERT INTO pokemon_sprites (user_id, sprite_url) VALUES ($userID, '$escapedSprite')";
+            $query = "INSERT INTO pokemon_order (user_id, pokemon_id, order_number) VALUES ($user_ID, $escaped_pokemonID, $order)";
             $mysqli->query($query);
+
+            $order++;
         }
 
         // Redirect to login.php
