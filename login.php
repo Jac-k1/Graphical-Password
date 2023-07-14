a519e53a5b92ae3b5df4e2cbd9abe9217ed585c2
Sadie-Jane <sadiejaneb@gmail.com>
2023-07-14 13:13:57 -0400

    updated login.php and added verify.php to verify pokemon

--- login.php
+++ login.php
@@ -14,122 +14,48 @@ if ($mysqli->connect_errno) {
     die("Failed to connect to MySQL: " . $mysqli->connect_error);
 }
 
-function fetchData($url) {
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $data = curl_exec($curl);
-    curl_close($curl);
-    return $data;
-}
-
-// Check if the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve the submitted username and password
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Validate the username and password
     if (empty($username) || empty($password)) {
-        echo '<script>alert("Username and password are required.");</script>';
-    } else {
-        // Check if the username and password match a record in the database
-        $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        echo "Please enter both username and password.";
+        exit();
+    }
+
+    // Check if the username and password match
+    $query = "SELECT user_id FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $mysqli->query($query);
+
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        $user_ID = $row['user_id'];
+
+        // Retrieve the batches the user needs to verify
+        $query = "SELECT DISTINCT order_number FROM pokemon_order WHERE user_id = $user_ID";
         $result = $mysqli->query($query);
 
-        if ($result->num_rows === 1) {
-            // Username and password are correct
-
-            // Retrieve the selected Pokémon for the user from the database
-            $row = $result->fetch_assoc();
-            $userID = $row['id'];
-
-            // Retrieve the selected Pokémon from the form
-            $selectedPokemon = $_POST['selected_pokemon'];
-
-            // Retrieve the Pokémon sprites stored in the database for the user
-            $query = "SELECT sprite_url FROM pokemon_sprites WHERE user_id = $userID";
-            $result = $mysqli->query($query);
-            $storedPokemon = [];
-
-            while ($row = $result->fetch_assoc()) {
-                $storedPokemon[] = $row['sprite_url'];
-            }
-
-            // Check if the selected Pokémon matches the stored Pokémon
-            if (count($selectedPokemon) === count($storedPokemon) && array_diff($selectedPokemon, $storedPokemon) === []) {
-                // Selected Pokémon match the stored Pokémon
-
-                // Store the username in the session
-                $_SESSION['username'] = $username;
-
-                // Redirect to authenticated_page.php
-                header("Location: authenticated_page.php");
-                exit;
-            } else {
-                // Selected Pokémon do not match the stored Pokémon
-                echo '<script>alert("Incorrect Pokémon selection.");</script>';
-            }
-        } else {
-            // Invalid username or password
-            echo '<script>alert("Invalid username or password.");</script>';
+        $batches = [];
+        while ($row = $result->fetch_assoc()) {
+            $batches[] = $row['order_number'];
         }
+
+        // Redirect to the verification page with the user ID and batches
+        $_SESSION['user_id'] = $user_ID;
+        $_SESSION['batches'] = $batches;
+        header("Location: verify.php");
+        exit();
+    } else {
+        echo "Invalid username or password.";
+        exit();
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <script>
-        function validateForm() {
-            var username = document.forms["loginForm"]["username"].value;
-            var password = document.forms["loginForm"]["password"].value;
-            var selectedPokemon = document.querySelectorAll('input[name="selected_pokemon[]"]:checked');
-
-            if (username === '' || password === '') {
-                alert('Username and password are required.');
-                return false;
-            }
-
-            if (selectedPokemon.length < 4) {
-                alert('Please select at least 4 Pokémon.');
-                return false;
-            }
-        }
-    </script>
-</head>
-<body>
-    <h2>Login</h2>
-    <form name="loginForm" method="post" onsubmit="return validateForm()">
-        Username: <input type="text" name="username"><br>
-        Password: <input type="password" name="password"><br>
-
-        <!-- Display the Pokémon sprites in checkboxes for selection -->
-        <?php
-        $pokemonListUrl = 'https://pokeapi.co/api/v2/pokemon?limit=30';
-        $pokemonListData = fetchData($pokemonListUrl);
-        $pokemonList = json_decode($pokemonListData)->results;
-
-        $pokemonDetails = [];
-        foreach ($pokemonList as $pokemon) {
-            $pokemonUrl = $pokemon->url;
-            $pokemonData = fetchData($pokemonUrl);
-            $pokemonDetails[] = json_decode($pokemonData);
-        }
-
-        foreach ($pokemonDetails as $pokemon) {
-            $pokemonSprite = $pokemon->sprites->front_default;
-            $pokemonName = $pokemon->name;
-
-            echo '<label>';
-            echo '<input type="checkbox" name="selected_pokemon[]" value="' . $pokemonSprite . '">';
-            echo '<img src="image-proxy.php?url=' . urlencode($pokemonSprite) . '" alt="' . $pokemonName . '">';
-            echo '</label>';
-        }
-        ?>
-
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
+<!-- Login form -->
+<form method="post">
+    <input type="text" name="username" placeholder="Enter your username" required>
+    <input type="password" name="password" placeholder="Enter your password" required>
+    <input type="submit" value="Login">
+</form>
---
+++ verify.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+
+$hostname = 'localhost';
+$username = 'sbuytendorp1';
+$password = 'sbuytendorp1';
+$database = 'sbuytendorp1';
+
+// Create a new MySQLi instance
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+// Check if the connection was successful
+if ($mysqli->connect_errno) {
+    die("Failed to connect to MySQL: " . $mysqli->connect_error);
+}
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['batches'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$user_ID = $_SESSION['user_id'];
+$batches = $_SESSION['batches'];
+
+// Check if all batches have been verified
+if (count($batches) === 1) {
+    // Verify the selected Pokémon against the database
+    $errors = verifyPokemonOrder($mysqli, $_SESSION['verification_results']);
+    if (count($errors) === 1) {
+        echo "Errors occurred during verification. Please try again.";
+    } else {
+        echo "All batches have been successfully verified.";
+        header("Location: authenticated_page.php");
+        
+    }
+    unset($_SESSION['verification_results']);
+    exit();
+}
+
+$batch_number = $batches[0];
+
+// Retrieve the Pokémon data for the current batch
+$url = "https://pokeapi.co/api/v2/pokemon?limit=50";
+$pokemon_data = json_decode(file_get_contents($url), true)['results'];
+?>
+
+<!-- Display the Pokémon sprites for verification -->
+<form method="post">
+    <p>Please select 1 Pokémon:</p>
+    <?php
+    foreach ($pokemon_data as $pokemon) {
+        $pokemon_id = extractPokemonIdFromUrl($pokemon['url']);
+        $pokemon_name = $pokemon['name'];
+        $pokemon_sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokemon_id.png";
+        ?>
+        <label>
+            <input type="radio" name="selected_pokemon" value="<?php echo $pokemon_id; ?>">
+            <img src="image-proxy.php?url=<?php echo urlencode($pokemon_sprite); ?>" alt="<?php echo $pokemon_name; ?>">
+        </label>
+        <?php
+    }
+    ?>
+    <br><br>
+    <input type="submit" value="Verify">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve the selected Pokémon ID
+    $selected_pokemon_id = $_POST['selected_pokemon'];
+
+    if (empty($selected_pokemon_id)) {
+        echo "Please select a Pokémon.";
+        exit();
+    }
+
+    // Store the verification result in the session array
+    $_SESSION['verification_results'][$batch_number] = $selected_pokemon_id;
+
+    // Remove the verified batch from the list
+    array_shift($batches);
+    $_SESSION['batches'] = $batches;
+
+    if (count($batches) === 0) {
+        // Verify the selected Pokémon against the database
+        $errors = verifyPokemonOrder($mysqli, $_SESSION['verification_results']);
+        if (count($errors) === 0) {
+            echo "All Pokemon have been successfully verified.";
+        } else {
+            echo "Errors occurred during verification. Please try again.";
+        }
+        unset($_SESSION['verification_results']);
+        exit();
+    } else {
+        // Redirect to the same page to verify the next batch
+        header("Location: verify.php");
+        exit();
+    }
+}
+
+function extractPokemonIdFromUrl($url)
+{
+    $parts = explode('/', rtrim($url, '/'));
+    return end($parts);
+}
+
+function verifyPokemonOrder($mysqli, $verification_results)
+{
+    $errors = [];
+
+    $user_ID = $_SESSION['user_id'];
+
+    foreach ($verification_results as $batch_number => $selected_pokemon_id) {
+        $query = "SELECT pokemon_id FROM pokemon_order WHERE user_id = $user_ID AND order_number = $batch_number";
+        $result = $mysqli->query($query);
+
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $pokemon_order = $row['pokemon_id'];
+
+            if ($selected_pokemon_id != $pokemon_order) {
+                $errors[] = "Verification failed";
+            }
+        } else {
+            $errors[] = "Failed to retrieve Pokémon.";
+        }
+    }
+
+    return $errors;
+}
+?>
